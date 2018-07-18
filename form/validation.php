28a6d42971f0d2feded4df62c917e39d28020023
Seifeddine KHALFA <khalfaseifeddine@gmail.com>
2018-07-18 16:26:08 +0100

    Validation php

--- form/validation.php
+++ form/validation.php
@@ -1,7 +1,7 @@
 <?php
 	$errorInputs = [];
 	$requireElements = array("name", "DateOfBirth", "PlaceOfBirth", "Adresse", "Phone", "E_mail", "GitHub_Profile", "Description");
-if($_POST)
+	if($_POST)
 	{
 		$name= $_POST['name'];
 		$DateOfBirth=$_POST['DateOfBirth'];
@@ -23,39 +23,22 @@ if($_POST)
 
 		if (count($errorInputs)==0) 
 		{
-			/*include 'cnx.php';
-			$sql = "INSERT INTO profile (name, DateOfBirth, PlaceOfBirth, Adresse, Phone, E_mail, GitHub_Profile, Description)
-			VALUES ('$name','$DateOfBirth', '$PlaceOfBirth', '$Adresse', '$Phone', '$E_mail', '$GitHub_Profile', '$Description')";
-			$bd->query($sql);
-			$bd->exec($dbh);*/
 			
-		
-			$servername = "localhost";
-			$username = "root";
-			$password = "";
-			$dbname = "cv";
-
-			try {
-				$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-				// set the PDO error mode to exception
-				$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-				$sql = "INSERT INTO profile (name, DateOfBirth, PlaceOfBirth, Adresse, Phone, E_mail, GitHub_Profile, Description)
-			VALUES ('$name','$DateOfBirth', '$PlaceOfBirth', '$Adresse', '$Phone', '$E_mail', '$GitHub_Profile', '$Description')";
+			
+			include"connexion.php";
+			
+			$sql = "INSERT INTO profile (name, DateOfBirth, PlaceOfBirth, Adresse, Phone, E_mail, GitHub_Profile, Description)
+				VALUES ('$name','$DateOfBirth', '$PlaceOfBirth', '$Adresse', '$Phone', '$E_mail', '$GitHub_Profile', '$Description')";
 				// use exec() because no results are returned
 				$conn->exec($sql);
 				//style***
 				echo "New record created successfully";
-				}
-			catch(PDOException $e)
-				{
-				echo $sql . "<br>" . $e->getMessage();
-				}
+				header("location:formulaire.php");
+				
+			
+				
 
 			$conn = null;
-			
-			
-
-			
 		}	
 		
 	
